b879ef0c68bbc013f61b90f92eb05171584f4908
loveworldlovesky <zhouwenchao8426@126.com>
2017-09-18 17:08:37 +0800

    quatz examples

---
+++ jeesky/src/main/java/demos/quartz/MyJob.java
@@ -0,0 +1,17 @@
+package demos.quartz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyJob implements Job{
+	private static Logger log = LoggerFactory.getLogger(MyJob.class);
+	@Override
+	public void execute(JobExecutionContext context)
+			throws JobExecutionException {
+		log.info("hello,我的第一个任务调度程序");
+	}
+
+}
